e676a066ade65e62cfa6f0f6e7948c02ca45b0fa
Austin Papritz <austin@papritz.dev>
2023-02-07 18:07:37 -0800

    removed hook from app, you can find it in Stories component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { useStories } from './hooks/useStories.js';
 import Stories from './components/Story/Stories.js';
 
 function App() {
-  const { stories } = useStories();
-
   return (
     <div className="App">
       <Stories />
