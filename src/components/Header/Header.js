77f9b1ad51b4e5ff11e36ef6850c6ade105b09b9
Austin Papritz <austin@papritz.dev>
2023-02-08 15:15:58 -0800

    sign out now redirects to signin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { signOut } from '../../services/auth.js';
 import './Header.css';
 
 export default function Header() {
+  const history = useHistory();
   return (
     <header>
-      <button onClick={signOut}>Log out</button>
+      <button
+        onClick={() => {
+          signOut();
+          history.push('/auth/sign-in');
+        }}
+      >
+        Log out
+      </button>
     </header>
   );
 }
