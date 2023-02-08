307a12ba1e38243e87b800e6fdd656210cefc464
Austin Papritz <austin@papritz.dev>
2023-02-08 14:27:08 -0800

    added signout button to header but not sure if working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { signOut } from '../../services/auth.js';
 import './Header.css';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <button onClick={signOut}>Log out</button>
+    </div>
+  );
 }
