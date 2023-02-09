d77de5b18626fa5d3b2ac33b10d6f7c4bc6604f4
Austin Papritz <austin@papritz.dev>
2023-02-08 16:01:39 -0800

    added NewStory component, need createNewStory in services

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Stories from './components/Story/Stories.js';
 
 import Header from './components/Header/Header.js';
 import Auth from './components/Auth/Auth.js';
+import NewStory from './components/Story/NewStory.js';
 
 import { Redirect, Route, Switch } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/auth/:type" component={Auth} />
         <Route exact path="/entries" component={Stories} />
+        <Route exact path="/entries/new" component={NewStory} />
         <Route path="*">
           <Redirect to="/auth/sign-in" />
         </Route>
---
+++ src/components/Story/NewStory.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NewStory() {
+  return <div>NewStory</div>;
+}
