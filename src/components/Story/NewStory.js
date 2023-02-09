b025e66bdf058b7331791edc0a804b8f90ec549d
Austin Papritz <austin@papritz.dev>
2023-02-08 17:31:51 -0800

    NewStory submits entry to supabase!

--- src/components/Story/NewStory.js
+++ src/components/Story/NewStory.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { createNewStory } from '../../services/stories.js';
 import StoryForm from './StoryForm.js';
 
 export default function NewStory() {
   const history = useHistory();
-  const handleSubmit = async (title, name, entry) => {};
+  const handleSubmit = async (title, name, entry) => {
+    await createNewStory(title, name, entry);
+  };
   return (
     <>
       <StoryForm submitHandler={handleSubmit} />
