35b4fe6cb64c94d5236226aa4e69fc029653992c
Austin Papritz <austin@papritz.dev>
2023-02-08 16:58:57 -0800

    made very simple NewStory.js that logs to console when PostForm is submitted

--- src/components/Story/NewStory.js
+++ src/components/Story/NewStory.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function NewStory() {
-  return <div>NewStory</div>;
+  const history = useHistory();
+  const handleSubmit = async (title, name, entry) => {
+    console.log('submit handle');
+  };
+  return (
+    <>
+      <PostForm submitHandler={handleSubmit} />
+    </>
+  );
 }
