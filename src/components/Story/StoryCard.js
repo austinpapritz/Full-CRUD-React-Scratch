ff455233c895c66b5a1a993abb7944db9f4665b0
Austin Papritz <austin@papritz.dev>
2023-02-11 14:41:24 -0800

    handleDelete function in StoryCard deletes story

--- src/components/Story/StoryCard.js
+++ src/components/Story/StoryCard.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Stories.css';
 import { useUser } from '../../context/UserContext.js';
+import { deleteStoryByID } from '../../services/stories.js';
 
 export default function StoryCard({ user_id, id, title, name, entry }) {
   const { user } = useUser();
   const owner = user.id === user_id;
+
+  async function handleDelete(id) {
+    await deleteStoryByID(id);
+  }
 
   return (
     <div className="scroll-div">
@@ -14,9 +19,20 @@ export default function StoryCard({ user_id, id, title, name, entry }) {
         <h3>by {name}</h3>
         <p>{entry}</p>
         {owner && (
-          <>
-            <Link to={`/entries/edit/${id}`}>Edit</Link>
-          </>
+          <div className="control">
+            <ul>
+              <li>
+                <Link to={`/entries/edit/${id}`}>Edit</Link>
+              </li>
+              <li
+                onClick={() => {
+                  handleDelete(id);
+                }}
+              >
+                <Link to={`/entries/delete/${id}`}>Delete</Link>
+              </li>
+            </ul>
+          </div>
         )}
       </div>
     </div>
