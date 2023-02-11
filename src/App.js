291006d76f60e6f63b35e5f3970e9569567d3caa
Austin Papritz <austin@papritz.dev>
2023-02-11 12:26:45 -0800

    added transparency div

--- src/App.js
+++ src/App.js
@@ -11,16 +11,18 @@ import EditStory from './components/Story/EditStory.js';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/auth/:type" component={Auth} />
-        <Route exact path="/entries/edit/:id" component={EditStory} />
-        <Route exact path="/entries" component={Stories} />
-        <Route exact path="/entries/new" component={NewStory} />
-        <Route path="*">
-          <Redirect to="/auth/sign-in" />
-        </Route>
-      </Switch>
+      <div className="transparency">
+        <Header />
+        <Switch>
+          <Route exact path="/auth/:type" component={Auth} />
+          <Route exact path="/entries/edit/:id" component={EditStory} />
+          <Route exact path="/entries" component={Stories} />
+          <Route exact path="/entries/new" component={NewStory} />
+          <Route path="*">
+            <Redirect to="/auth/sign-in" />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
