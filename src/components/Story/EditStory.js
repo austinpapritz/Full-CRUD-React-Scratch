9eef3b89d585a3c72c0ab0ea45b1d2f587fa75d7
Austin Papritz <austin@papritz.dev>
2023-02-10 17:22:03 -0800

    EditStory.js component edits story. BUG replaces with empty field

--- src/components/Story/EditStory.js
+++ src/components/Story/EditStory.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useStory } from '../../hooks/useStory.js';
+import { updateStory } from '../../services/stories.js';
+import StoryCard from './StoryCard.js';
+import StoryForm from './StoryForm.js';
 
 export default function EditStory() {
-  return <div>EditStory</div>;
+  const { id } = useParams();
+  const { storyDetail, error, setError } = useStory(id);
+
+  if (error) return <h1>{error}</h1>;
+
+  const handleSubmit = async (title, name, entry) => {
+    try {
+      await updateStory(storyDetail.id, title, name, entry);
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <>
+      <StoryForm {...storyDetail} submitHandler={handleSubmit} />
+      <StoryCard {...storyDetail} />
+    </>
+  );
 }
