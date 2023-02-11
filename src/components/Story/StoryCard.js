41ee13292e9603700908bccab95c5fdbdc85563e
Austin Papritz <austin@papritz.dev>
2023-02-10 18:35:25 -0800

    StoryCard takes user_id and id to verify owner of card and generate an edit link accordingly

--- src/components/Story/StoryCard.js
+++ src/components/Story/StoryCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Stories.css';
+import { useUser } from '../../context/UserContext.js';
 
-export default function StoryCard({ title, name, entry }) {
+export default function StoryCard({ user_id, id, title, name, entry }) {
+  const { user } = useUser();
+  const owner = user.id === user_id;
+
   return (
     <div className="scroll-div">
       <h3>
         {title} by {name}
       </h3>
       <p>{entry}</p>
+      {owner && (
+        <>
+          <Link to={`/entries/edit/${id}`}>Edit</Link>
+        </>
+      )}
     </div>
   );
 }
