13bf8b2562bf4727319b994f5d02197e44829dd7
Austin Papritz <austin@papritz.dev>
2023-02-13 13:25:14 -0800

    set up route and link for clicking character

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ import Header from './components/Header/Header.js';
 import Auth from './components/Auth/Auth.js';
 import NewStory from './components/Story/NewStory.js';
 
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
 import EditStory from './components/Story/EditStory.js';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <img className="mary" src={process.env.PUBLIC_URL + '/mary.png'}></img>
-      <img className="bandit" src={process.env.PUBLIC_URL + '/bandit.png'}></img>
+      <Link to="/entries/mary">
+        <img className="mary" src={process.env.PUBLIC_URL + '/mary.png'} />
+      </Link>
+      <Link to="/entries/martin">
+        <img className="bandit" src={process.env.PUBLIC_URL + '/bandit.png'} />
+      </Link>
+
       <Switch>
         <Route exact path="/">
           <Header main={true} />
         </Route>
-        <Route exact path="/auth/:type" component={Auth} />
-        <Route exact path="/entries/edit/:id" component={EditStory} />
-        <Route exact path="/entries" component={Stories} />
-        <Route exact path="/entries/new" component={NewStory} />
+        <Route exact path="/auth/:type" component={Auth}>
+          <Header auth={true} />
+        </Route>
+        <Route exact path="/entries/edit/:id" component={EditStory}>
+          <Header edit={true} />
+        </Route>
+        <Route exact path="/entries/:name" component={Stories}>
+          <Header stories={true} />
+        </Route>
+        <Route exact path="/entries/new" component={NewStory}>
+          <Header new={true} />
+        </Route>
         <Route path="*">
           <Redirect to="/auth/sign-in" />
         </Route>
